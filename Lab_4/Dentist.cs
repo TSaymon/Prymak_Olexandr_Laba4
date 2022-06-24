b71255c4f4ea6988d6018e997b02beb34ec28446
TSaymon <105799546+TSaymon@users.noreply.github.com>
2022-06-25 01:03:46 +0300

    Add files via upload

---
+++ Lab_4/Dentist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OP4
+{
+    public class Dentist : Doctor, IStomatology
+    {
+        public Dentist(int salary) : base(salary)
+        {
+        }
+
+        public int FixTooth(int countOfFix)
+        {
+            return YearOfWork * Salary * countOfFix;
+        }
+
+        public override void ShowInformation()
+        {
+            Console.WriteLine($"I am a dentist, with a {YearOfWork} years of work, my salary is: {Salary}");
+        }
+    }
+}
